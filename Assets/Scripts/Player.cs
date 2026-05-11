507c57cf70890322beb89fa3d37d52a7de15c370
XC1976 <contact@chankyle.com>
2026-05-11 15:26:46 +0200

    style: 🎨 Added animations (idle, running, jump, fall)
    Detect each frame if the player is idle, running, jumping or falling and
    set the appropriate animation.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,12 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public float moveSpeed = 5f;
 
     // === Properties === //
+    
     private Rigidbody2D _playerRigidBody; // Rigidbody of the player
     public float jumpForce = 10f;   // The force used for jump
 
     // === These properties are used to stop the player from jumping infinitely in the air. === //
+     
     // We created a small invisible GameObject (circle) at the player's feet (groundCheck).
     public Transform groundCheck;
     // Radius of the invisible circle
@@ -17,10 +19,18 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public LayerMask groundLayer;
     // true if grounded, false if not
     private bool _isGrounded;
+
+    // === Propreties linked to animation === //
+
+    private Animator _animator;
     
     void Start()
     {
+        // Get the Rigidbody2D of the player
         _playerRigidBody = GetComponent<Rigidbody2D>();
+
+        // Get the Animator component
+        _animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -41,6 +51,9 @@ public class NewMonoBehaviourScript : MonoBehaviour
             // Same logic as left and right, the first argument does not affect anything, the second jumps
             _playerRigidBody.linearVelocity = new Vector2(_playerRigidBody.linearVelocity.x, jumpForce);
         }
+
+        // Check which animation to play
+        SetAnimation(moveInput);
     }
 
     private void FixedUpdate()
@@ -49,5 +62,38 @@ public class NewMonoBehaviourScript : MonoBehaviour
         // If if has a collision with a GameObject Layer 6 : Ground
         // Will store true if yes, false if not (in the air, falling...)
         _isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    // Function to check which animation to play on which circumstances
+    private void SetAnimation(float moveInput)
+    {
+        // All the scenarios where the player is on the ground
+        if (_isGrounded)
+        {
+            // This means the player is on the ground and not walking (neither -1 or 1), play "Player_idle"
+            if (moveInput == 0)
+            {
+                _animator.Play("Player_idle");
+            }
+            // This means the player is grounded and moving
+            else
+            {
+                _animator.Play("Player_run");
+            }
+        }
+        // The player is not on the ground
+        else
+        {
+            // If y > 0, it means the player is going up (jump)
+            if (_playerRigidBody.linearVelocityY > 0)
+            {
+                _animator.Play("Player_jump");
+            }
+            // That means y < 0, so the player is falling
+            else
+            {
+                _animator.Play("Player_fall");
+            }
+        }
     }
 }
